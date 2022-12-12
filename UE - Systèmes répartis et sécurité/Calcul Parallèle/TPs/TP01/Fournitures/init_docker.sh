f055a6d2c7d385cdb0e704fa619ed23800715384
gdai_on_linux_n7 <gdai@nickel.enseeiht.fr>
2022-12-12 16:56:45 +0100

    UPDATE

---
+++ UE - Systèmes répartis et sécurité/Calcul Parallèle/TPs/TP01/Fournitures/init_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# for docker
+SIMGRID=/source/workspace/
+
+export PATH=${SIMGRID}/bin:${PATH}
+
+alias smpirun="smpirun -hostfile ${SIMGRID}/archis/cluster_hostfile.txt -platform ${SIMGRID}/archis/cluster_crossbar.xml"
--- UE - Systèmes répartis et sécurité/Calcul Parallèle/TPs/TP01/Fournitures/init_n7.sh
+++ UE - Systèmes répartis et sécurité/Calcul Parallèle/TPs/TP01/Fournitures/init_n7.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SIMGRID=/source/workspace/
+SIMGRID=/mnt/n7fs/ens/tp_guivarch/opt2021/simgrid-3.31
 
 export PATH=${SIMGRID}/bin:${PATH}
 
